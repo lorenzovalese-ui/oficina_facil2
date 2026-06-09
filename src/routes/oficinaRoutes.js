6a34d12698abf673494b99bd10adb504ac59bfea
LORENZO OLIVEIRA VALESE <lorenzo_valese@estudante.sesisenai.org.br>
2026-06-09 10:46:35 -0300

    docs: atualiza projeto

--- src/routes/oficinaRoutes.js
+++ src/routes/oficinaRoutes.js
@@ -43,4 +43,70 @@ router.get('/clientes/:id', async (req, res) => {
     res.status(200).json(cliente);
 });
 
+// DELETE - Remover cliente
+router.delete('/clientes/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+
+        const cliente = await prisma.cliente.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if (!cliente) {
+            return res.status(404).json({
+                mensagem: 'Cliente não encontrado'
+            });
+        }
+
+        await prisma.cliente.delete({
+            where: {
+                id: id
+            }
+        });
+
+        return res.status(204).send();
+
+    } catch (erro) {
+        return res.status(500).json({
+            erro: erro.message
+        });
+    }
+});
+
+// PUT - Atualizar cliente
+router.put('/clientes/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const { nome, email, telefone } = req.body;
+
+        const cliente = await prisma.cliente.findUnique({
+            where: { id }
+        });
+
+        if (!cliente) {
+            return res.status(404).json({
+                mensagem: 'Cliente não encontrado'
+            });
+        }
+
+        const clienteAtualizado = await prisma.cliente.update({
+            where: { id },
+            data: {
+                nome,
+                email,
+                telefone
+            }
+        });
+
+        res.status(200).json(clienteAtualizado);
+
+    } catch (erro) {
+        res.status(500).json({
+            erro: erro.message
+        });
+    }
+});
+
 module.exports = router;
